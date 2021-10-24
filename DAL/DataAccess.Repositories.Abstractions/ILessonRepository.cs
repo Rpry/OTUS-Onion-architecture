05ee6735d43e105131a2d3795e96f939f1d577b5
1 <1>
2021-10-24 12:52:21 +0300

    Project was created

---
+++ DAL/DataAccess.Repositories.Abstractions/ILessonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataAccess.Abstraction;
+using DataAccess.Entities;
+
+namespace DataAccess.Repositories
+{
+    /// <summary>
+    /// Репозиторий работы с уроками
+    /// </summary>
+    public interface ILessonRepository: IRepository<Lesson, int>
+    {
+        Task<List<Lesson>> GetPagedAsync(int page, int itemsPerPage);
+    }
+}
